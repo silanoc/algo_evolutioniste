e1e5abada3f01fa83f9ced814e48a115415ea2d1
silanoc <silanoc@gmail.com>
2022-09-22 10:36:16 +0200

    creation db, table et systeme de log

--- domaine/port_sql.py
+++ domaine/port_sql.py
@@ -1,15 +1,46 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+from cgitb import lookup
 import sqlite3
+import logging
 
-def connection_a_la_base(chemin = 'interface/sqlite/db_sequence.db'):
+from multiprocessing import connection
+
+
+CHEMIN = 'interface/sqlite/db_sequence.db'
+
+logging.basicConfig(level = logging.DEBUG,
+                    filename = "interface/mes_logs/port_sql.log",
+                    filemode = "a",
+                    format = '%(asctime)s -%(levelname)s - %(message)s')
+
+def connection_a_la_base(chemin = CHEMIN):
     try:
         connection = sqlite3.connect(chemin)
-        print('connection à la base ok')
+        logging.debug("connection à la base ok")
     except:
-        print('erreur de connexion')
+        logging.error("erreur de connexion")
     return connection
 
-print(connection_a_la_base())
+def creation_table_sequence(chemin = CHEMIN):
+    try:
+        connection = connection_a_la_base(chemin)
+        curseur = connection.cursor()
+        table_sequence = '''CREATE TABLE IF NOT EXISTS Tb_sequence(
+            ID INTEGER,
+            Naissance INTEGER,
+            Age INTEGER,
+            Etat INTEGER,
+            Sequence TEXT,
+            Score_lettre REAL,
+            Score_bigram REAL,
+            Score_total REAL     
+        )'''
+        curseur.execute(table_sequence)
+        logging.debug('création base ok')
+    except:
+        logging.error('base non créée')    
 
+
+creation_table_sequence()
--- interface/test/test_port_sql.py
+++ interface/test/test_port_sql.py
@@ -6,3 +6,6 @@ from domaine.port_sql import *
 def test_connection_a_la_base():
     conn = connection_a_la_base()
     assert type(conn) == sqlite3.Connection
+    
+def test_creation_base():
+    pass
