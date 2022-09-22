8f967b9411763435bbdcbab703d149023706784f
silanoc <silanoc@gmail.com>
2022-09-22 10:02:24 +0200

    ouverture bdd et test

--- domaine/port_sql.py
+++ domaine/port_sql.py
@@ -1,3 +1,15 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import sqlite3
+
+def connection_a_la_base(chemin = 'interface/sqlite/db_sequence.db'):
+    try:
+        connection = sqlite3.connect(chemin)
+        print('connection à la base ok')
+    except:
+        print('erreur de connexion')
+    return connection
+
+print(connection_a_la_base())
+
---
+++ interface/test/test_port_sql.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from domaine.port_sql import *
+
+def test_connection_a_la_base():
+    conn = connection_a_la_base()
+    assert type(conn) == sqlite3.Connection
